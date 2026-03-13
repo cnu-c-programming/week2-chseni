7e56650476e10302f96321b849fd22c9ea1de3dd
JeongChan Lee <110677779+jclee2004@users.noreply.github.com>
2026-03-13 12:17:31 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -5,7 +5,7 @@ int main() {
 
     printf("int : %d\n", var1);
     printf("hex : %x\n", var1);
-    printf("int : %o\n", var1);
+    printf("oct : %o\n", var1);
 
     char var2 = 'c';
     printf("char : %c\n", var2);
