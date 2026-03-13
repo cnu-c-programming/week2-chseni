75da3d75eeff78d9e16885713822bbd8b4db00d5
JeongChan Lee <110677779+jclee2004@users.noreply.github.com>
2026-03-13 12:14:06 +0900

    Implement prime number check in main function
    Added functionality to check if a number is prime.

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
-int main()
-{
-
+int main() {
+    int a;
+    scanf("%d",&a);
+    
+    int b = sqrt(a);
+    int c = 0;
+    for(int i=2;i<=b;i++) {
+        if(a%i==0) {
+            printf("false\n");
+            c = 1;
+            break;
+        }
+    }
+    if(c==0) printf("true\n");
     return 0;
-}
 
+}
